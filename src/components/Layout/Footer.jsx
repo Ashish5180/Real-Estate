7c7e0f2e729c3c841db997dd648440089d3a8106
Ashish Yadav <ashish.y8750@gmail.com>
2025-04-04 21:21:29 +0530

    Update Footer.jsx

--- src/components/Layout/Footer.jsx
+++ src/components/Layout/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer className="bg-gradient-to-r from-[#feabce] to-[#aaa4fd] opacity-70 py-4">
       <div className="container mx-auto text-center text-black">
-        <p>&copy; {new Date().getFullYear()} Ashish Yadav.inc All Rights Reserved</p>
+        <p>&copy; {new Date().getFullYear()}.inc All Rights Reserved</p>
       </div>
     </footer>
   );
